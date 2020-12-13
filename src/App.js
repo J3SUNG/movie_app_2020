495a2bb8e5b620180af231af4b064db9d52736b4
J3SUNG <wptjd6141@naver.com>
2020-12-13 04:21:05 -0800

    setState, current

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
+  minus = () => {
+    this.setState((current) => ({
+      count: current.count - 1,
+      s,
+    }));
+  };
   render() {
-    return <h1>Im a class component</h1>;
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
 }
 
