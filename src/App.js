84305c3604d83ee1401331a90828c790ba9ab987
J3SUNG <wptjd6141@naver.com>
2020-12-12 16:22:26 -0800

    Dynamic component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
-import Potato from "./Potato";
+
+const palete = [
+  { color: "purple" },
+  { color: "blue" },
+  { color: "green" },
+  { color: "yellow" },
+];
+
+function Potato({ color }) {
+  console.log(color);
+  return <div>I like {color} potato!</div>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Potato></Potato>
+      {palete.map((v) => (
+        <Potato color={v.color} />
+      ))}
+      <Potato color="red" count="5" price="{[3, 2, 4, 3, 1]}"></Potato>
+      <Potato color="blue" count="5" price="{[3, 2, 4, 3, 1]}"></Potato>
+      <Potato color="green" count="5" price="{[3, 2, 4, 3, 1]}"></Potato>
+      <Potato color="yellow" count="5" price="{[3, 2, 4, 3, 1]}"></Potato>
     </div>
   );
 }
