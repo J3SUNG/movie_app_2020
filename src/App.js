9d5f4725ab0afeb11c4f2d3ea382e1ca7ab4753f
J3SUNG <wptjd6141@naver.com>
2020-12-13 23:36:05 -0800

    print movie data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-
+import Movie from "./Movie";
 class App extends React.Component {
   state = {
     isLoading: true,
@@ -11,15 +11,39 @@ class App extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
     console.log(movies);
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
