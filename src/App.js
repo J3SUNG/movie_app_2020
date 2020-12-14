28063a087408fa0fdc689aa7461fa0bd6ccab54f
J3SUNG <wptjd6141@naver.com>
2020-12-13 22:44:43 -0800

    install axios

--- src/App.js
+++ src/App.js
@@ -3,36 +3,17 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0,
-  };
-  add = () => {
-    this.setState((current) => ({
-      count: current.count + 1,
-    }));
-  };
-  minus = () => {
-    this.setState((current) => ({
-      count: current.count - 1,
-    }));
+    isLoading: true,
+    movies: [],
   };
   componentDidMount() {
-    console.log("component renderd");
-  }
-  componentDidUpdate() {
-    console.log("component update");
-  }
-  componentWillUnmount() {
-    console.log("component unmount");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
   }
   render() {
-    console.log("render");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
